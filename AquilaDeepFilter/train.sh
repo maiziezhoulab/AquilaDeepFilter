85658cdb000cb427940d594c27aafde8a221e632
Yunfei Hu <yunfei.hu@vanderbilt.edu>
2022-06-10 23:43:37 -0500

    Update train.sh

--- AquilaDeepFilter/train.sh
+++ AquilaDeepFilter/train.sh
@@ -7,3 +7,15 @@ nvidia-docker run -v /home:/home -it tensorflow/tensorflow:latest-gpu python /ho
     --model_arch efficientnet --fine_tune_at 40 --height 200 --width 400 --custom_input_preprocessing True
 
 #  "xception" "densenet" "resnet" "mobilenet" "mobilenetv1" "efficientnet" "vgg"
+
+
+singularity exec /data/maiziezhou_lab/sanidhya/docker_images/tf25-gpu.sif python /data/maiziezhou_lab/huyf/AquilaDeepFilter/AquilaDeepFilter/main.py train \
+--model_arch=resnet \
+--epoch=60 \
+--batch_size=128 \
+--lr=0.001 \
+--fine_tune_at=45 \
+--path_to_train_dir=*/use_phasing/10xweb/phased_train/ \
+--path_to_eval_dir=*/use_phasing/10xweb/phased_test/ \
+--checkpoint_dir=*/use_phasing/10xweb/resnet/ckpt/ \
+--tensorboard_log_dir=*/use_phasing/10xweb/resnet/summary/
