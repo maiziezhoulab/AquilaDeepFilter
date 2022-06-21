4a0b134a3c9cb9ae25c9b751d561128986c115cf
Yunfei Hu <yunfei.hu@vanderbilt.edu>
2022-06-21 13:25:00 -0500

    Update train.sh

--- AquilaDeepFilter/train.sh
+++ AquilaDeepFilter/train.sh
@@ -4,7 +4,7 @@ nvidia-docker run -v /home:/home -it tensorflow/tensorflow:latest-gpu python /ho
     --path_to_eval_dir  /home/yunfei/workspace/shortreads_data/experiment_data_shortreads_del/Aquila/NA24385_split_test \
     --checkpoint_dir /home/yunfei/workspace/weights/ckpt/efficientnet/ \
     --tensorboard_log_dir /home/yunfei/workspace/weights/tensorboard_logs/efficientnet/ \
-    --model_arch efficientnet --fine_tune_at 40 --height 200 --width 400 --custom_input_preprocessing True
+    --model_arch efficientnet --fine_tune_at 40
 
 #  "xception" "densenet" "resnet" "mobilenet" "mobilenetv1" "efficientnet" "vgg"
 
